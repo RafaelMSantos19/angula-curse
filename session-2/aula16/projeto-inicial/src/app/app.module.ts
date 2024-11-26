0a3e72ab37b7ce03a25df04f6583c6562eaf0281
Rafael Santos <rafarag40@gmail.com>
2024-11-26 06:28:38 -0300

    feat: create and use new angular components

--- session-2/aula16/projeto-inicial/src/app/app.module.ts
+++ session-2/aula16/projeto-inicial/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp2Component } from './comp2/comp2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp2Component
   ],
   imports: [
     BrowserModule,
